59a7be9f52fd177a38a5ba7593a7aa8cef0fd27f
samiahafez <samia.hafez@gmail.com>
2019-05-10 13:59:16 +0200

    Start...

---
+++ src/Helper.java
@@ -0,0 +1,6 @@
+
+public class Helper {
+	int timeInNetwork;
+	
+
+}
---
+++ src/Simulation.java
@@ -0,0 +1,16 @@
+
+public class Simulation {
+
+	int nofHelpers;
+	double malRatio;
+	int difficulty;
+	// in hours
+	int simTime;
+// 	public Simulation(){
+		
+	}
+	public void startSimulation(){
+		
+	}
+	
+}
